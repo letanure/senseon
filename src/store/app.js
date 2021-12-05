f2c79702d3adc1ddb236f25f8f03ae12b3082efe
Luiz Tanure <letanure@gmail.com>
2021-12-06 00:51:58 +0100

    chore: add prettier

--- src/store/app.js
+++ src/store/app.js
@@ -1,49 +1,54 @@
 import axios from 'axios'
 
-const API_BASE_URL = '/api';
-const API_POLL_INTERVAL_MS = 10000;
+const API_BASE_URL = '/api'
+const API_POLL_INTERVAL_MS = 10000
 
-let apiPollToken = null;
+let apiPollToken = null
 
 export const app = {
   namespace: 'app',
   actions: {
-    POLL_FOR_API: 'POLL_FOR_API'
+    POLL_FOR_API: 'POLL_FOR_API',
   },
   mutations: {
     SET_API_AVAILABLE: 'SET_API_AVAILABLE',
-    SET_API_MOCKED: 'SET_API_MOCKED'
-  }
-};
+    SET_API_MOCKED: 'SET_API_MOCKED',
+  },
+}
 
 export const state = () => ({
   apiAvailable: null,
-  apiMocked: null
-});
+  apiMocked: null,
+})
 
 export const actions = {
   [app.actions.POLL_FOR_API]({ commit, state }) {
-    if (apiPollToken) clearTimeout(apiPollToken);
+    if (apiPollToken) clearTimeout(apiPollToken)
 
-    const poll = () => axios.get(`${API_BASE_URL}`)
-      .then(response => {
-        if (!state.apiAvailable) {
-          commit(app.mutations.SET_API_AVAILABLE, response.data.success);
-          if (response.data.mocked) commit(app.mutations.SET_API_MOCKED, true);
-        }
-      })
-      .catch(() => state.apiAvailable && commit(app.mutations.SET_API_AVAILABLE, false))
-      .finally(() => apiPollToken = setTimeout(poll, API_POLL_INTERVAL_MS));
+    const poll = () =>
+      axios
+        .get(`${API_BASE_URL}`)
+        .then((response) => {
+          if (!state.apiAvailable) {
+            commit(app.mutations.SET_API_AVAILABLE, response.data.success)
+            if (response.data.mocked) commit(app.mutations.SET_API_MOCKED, true)
+          }
+        })
+        .catch(
+          () =>
+            state.apiAvailable && commit(app.mutations.SET_API_AVAILABLE, false)
+        )
+        .finally(() => (apiPollToken = setTimeout(poll, API_POLL_INTERVAL_MS)))
 
-    poll();
-  }
-};
+    poll()
+  },
+}
 
 export const mutations = {
   [app.mutations.SET_API_AVAILABLE](state, payload) {
-    state.apiAvailable = payload;
+    state.apiAvailable = payload
   },
   [app.mutations.SET_API_MOCKED](state, payload) {
-    state.apiMocked = payload;
-  }
+    state.apiMocked = payload
+  },
 }
--- src/store/counter.js
+++ src/store/counter.js
@@ -8,33 +8,33 @@ export const counter = {
   namespace: 'counter',
   actions: {
     DECREMENT: 'DECREMENT',
-    INCREMENT: 'INCREMENT'
+    INCREMENT: 'INCREMENT',
   },
   mutations: {
-    CHANGE_COUNTER: 'CHANGE_COUNTER'
-  }
-};
+    CHANGE_COUNTER: 'CHANGE_COUNTER',
+  },
+}
 
 // This defines the initial state
 export const state = () => ({
-  value: 0
-});
+  value: 0,
+})
 
 // Implementation of the store actions. Action can perform asynchronous tasks.
 export const actions = {
   // This action makes a commit directly to the store without any asynchronous calls.
   [counter.actions.DECREMENT]({ commit }) {
-    commit(counter.mutations.CHANGE_COUNTER, -1);
+    commit(counter.mutations.CHANGE_COUNTER, -1)
   },
   // This action commits a mutation in an asynchronous callback of a 10ms timeout.
   [counter.actions.INCREMENT]({ commit }) {
     setTimeout(() => commit(counter.mutations.CHANGE_COUNTER, +1), 10)
-  }
-};
+  },
+}
 
 // Implementation of the state mutations
 export const mutations = {
   [counter.mutations.CHANGE_COUNTER](state, delta) {
-    state.value += delta;
-  }
+    state.value += delta
+  },
 }
