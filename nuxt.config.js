f1e55dbd4caf3c474d9cf2ce2290d09c61193e03
Luiz Tanure <letanure@gmail.com>
2021-12-06 23:50:52 +0100

    chore: prettier nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,73 +1,69 @@
 export default {
-  srcDir: 'src/',
+  srcDir: "src/",
 
   server: {
     port: 3333, // Tests require port 3333 to be used
-    host: 'localhost' // The API mocking requires host to be localhost
+    host: "localhost" // The API mocking requires host to be localhost
   },
 
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-  mode: 'spa',
+  mode: "spa",
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'SenseOn Tech Test',
+    title: "SenseOn Tech Test",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/senseon-favicon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/senseon-favicon.png" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '~/plugins/api-mocking.js'
-  ],
+  plugins: ["~/plugins/api-mocking.js"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // '@nuxtjs/proxy',
-    '@nuxtjs/axios'
+    "@nuxtjs/axios"
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    extend(config, {isDev, isClient, isServer}) {
-      config.output.globalObject = 'this';
-      config.devtool = isClient ? 'eval-source-map' : 'inline-source-map';
+    extend(config, { isDev, isClient, isServer }) {
+      config.output.globalObject = "this";
+      config.devtool = isClient ? "eval-source-map" : "inline-source-map";
       config.node = {
-          fs: 'empty'
+        fs: "empty"
       };
 
       if (isClient) {
         config.module.rules.push({
-            test: /\.vue$/,
-            loader: "vue-svg-inline-loader",
-            options: { /* ... */ }
+          test: /\.vue$/,
+          loader: "vue-svg-inline-loader",
+          options: {
+            /* ... */
+          }
         });
       }
 
       if (!isDev && isServer) {
-          config.module.rules[0].query.loaders.css = 'css-loader'
+        config.module.rules[0].query.loaders.css = "css-loader";
       }
     }
   }
-}
+};
