4bf6b85f17d62f1cc650c545c10ecbc2f0478b57
Luiz Tanure <letanure@gmail.com>
2021-12-06 21:28:08 +0100

    chore: add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+};
