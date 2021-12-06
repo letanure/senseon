f31694d517d67447873b75546779fe0f40cf0c6f
Luiz Tanure <letanure@gmail.com>
2021-12-06 23:50:31 +0100

    chore: update default bg storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,19 @@ Vue.use(Vuex);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: "senseon",
+    values: [
+      {
+        name: "senseon",
+        value: "#174140"
+      },
+      {
+        name: "light",
+        value: "#fafafa"
+      }
+    ]
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
